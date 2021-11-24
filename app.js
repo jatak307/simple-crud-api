e2c32185357a7a6c98d7b49d2f8de409b27b825c
jatak307 <jatak307@gmail.com>
2021-11-24 19:31:13 +0300

    feat: create server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('eferferferf');
+const http = require('http');
+
+const persons = [];
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/persons' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(persons));
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify({message: 'Route Not Found'}));
+  }
+  
+});
+
+const PORT = process.env.PORT || 7000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

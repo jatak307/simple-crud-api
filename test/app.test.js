b57673a592edd31c4533ae2c78cc3e960c01791f
jatak307 <jatak307@gmail.com>
2021-11-26 18:57:00 +0300

    refactor: correct test descriptions, remove lines of unnecessary code

--- test/app.test.js
+++ test/app.test.js
@@ -6,17 +6,17 @@ const data = require('../src/data/persons.json');
 let persID = null;
 
 describe("HTTP server", function () {
-    test("Should get an array of data", async () => {
-        const expectedResponse = data;
+    test("Scenario #1. GET: should get an array of data", async () => {
         const response = await request(server).get('/persons');
-        if (expectedResponse.length > 0) {
-            expect(response.body).toEqual(expectedResponse);
+
+        if (data.length > 0) {
+            expect(response.body).toEqual(data);
         } else {
             expect(response.body).toEqual([]);
         }
     });
 
-    test("Should write a new person to the data array and get the object of the new person", async () => {
+    test("Scenario #2. POST: should write a new person to the data array and get the object of the new person", async () => {
         const newPerson = {
             "name": "Voltron",
             "age": 3000,
@@ -25,45 +25,42 @@ describe("HTTP server", function () {
         const response = await request(server).post('/persons').send(newPerson);
 
         expect(response.body.id).toBeDefined();
-        expect(response.body.name).toBe('Voltron');
-        expect(response.body.age).toBe(3000);
-        expect(response.body.hobbies).toEqual(["to save the world", "dancing"]);
+        expect(response.body.name).toBe(newPerson.name);
+        expect(response.body.age).toBe(newPerson.age);
+        expect(response.body.hobbies).toEqual(newPerson.hobbies);
     });
 
-    test("Should get an object by its ID", async () => {
+    test("Scenario #3. GET: should get an object by its ID", async () => {
         const somePerson = data[data.length - 1];
         persID = somePerson.id;
-        const somePersonID = somePerson.id;
-        const response = await request(server).get(`/persons/${somePersonID}`);
+        const response = await request(server).get(`/persons/${persID}`);
 
-        expect(response.body.id).toBe(somePerson.id);
+        expect(response.body.id).toBe(persID);
     });
 
-    test("Should update the previously created object. IDs of the original object and the updated one must match", async () => {
-        const somePerson = data[data.length - 1];
+    test("Scenario #4. PUT: should update the previously created object. IDs of the original object and the updated one must match", async () => {
         const updatePersonValues = {
             "name": "Venom",
             "hobbies": ["tearing off heads", "journalism"]
         };
-        const response = await request(server).put(`/persons/${somePerson.id}`).send(updatePersonValues);
+        const response = await request(server).put(`/persons/${persID}`).send(updatePersonValues);
 
-        expect(response.body.id).toBe(somePerson.id);
+        expect(response.body.id).toBe(persID);
         expect(response.body.name).toBe(updatePersonValues.name);
         expect(response.body.hobbies[1]).toBe(updatePersonValues.hobbies[1]);
     });
 
-    test("Should delete the object with the specified ID and receive a message about successful deletion", async () => {
-        const somePerson = data[data.length - 1];
-        const responseObj = { message: `Person ${somePerson.id} removed` };
+    test("Scenario #5. DELETE: should delete the object with the specified ID and receive a message about successful deletion", async () => {
+        const responseObj = { message: `Person ${persID} removed` };
+        const response = await request(server).delete(`/persons/${persID}`);
 
-        const response = await request(server).delete(`/persons/${somePerson.id}`);
         expect(response.body.message).toBe(responseObj.message);
     });
 
-    test("Should receive an answer that the person with the specified ID was not found", async () => {
+    test("Scenario #6. GET: should receive an answer that the person with the specified ID was not found", async () => {
         const responseObj = { message: `Person with ID ${persID} not found` };
-
         const response = await request(server).get(`/persons/${persID}`);
+
         expect(response.body.message).toBe(responseObj.message);
     });
 });

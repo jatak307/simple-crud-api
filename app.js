5a36bc008b7d3d45664e107af32ec9f9cb38462f
jatak307 <jatak307@gmail.com>
2021-11-26 16:47:29 +0300

    feat: specify an explicit port listening on the child object

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const server = require('./src/server');
 
 const PORT = process.env.PORT || 7000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (!module.parent) {
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}

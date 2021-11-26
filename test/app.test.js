3ce6f5a7c683f7b082c3ba705ae54fea3a477e93
jatak307 <jatak307@gmail.com>
2021-11-26 17:45:53 +0300

    feat: add a script for test # 2 (POST request creates a new object)

--- test/app.test.js
+++ test/app.test.js
@@ -12,6 +12,19 @@ describe("HTTP server", function () {
         } else {
             expect(response.body).toEqual([]);
         }
-        
+    });
+
+    test("Should write a new person to the data array and get the object of the new person", async () => {
+        const newPerson = {
+            "name": "Voltron",
+            "age": 3000,
+            "hobbies": ["to save the world", "dancing"]
+        };
+        const response = await request(server).post('/persons').send(newPerson);
+
+        expect(response.body.id).toBeDefined();
+        expect(response.body.name).toBe('Voltron');
+        expect(response.body.age).toBe(3000);
+        expect(response.body.hobbies).toEqual(["to save the world", "dancing"]);
     });
 });

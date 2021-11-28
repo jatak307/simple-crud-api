9f904b272274bf398667b89ef278a7aecf33d60a
jatak307 <jatak307@gmail.com>
2021-11-28 21:20:33 +0300

    fix: fix id-validation

--- src/server.js
+++ src/server.js
@@ -7,11 +7,12 @@ const server = http.createServer((req, res) => {
 
   const mainUrl = '/persons';
   const personUrl = /\/persons\/[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/;
+  const checkReq = urlArr.length > 2 && id.length !== 36;
 
   if (urlArr.length === 2 && req.url !== mainUrl) {
     res.writeHead(500, { 'Content-Type': 'application/json'});
     res.end(JSON.stringify({message: 'No trabaja!'}));
-  } else if (urlArr.length > 2 && id.length !== 36) {
+  } else if (checkReq) {
     res.writeHead(400, { 'Content-Type': 'application/json'});
     res.end(JSON.stringify({message: `ID ${id} is not valid`}));
   } else if (req.url === mainUrl) {
